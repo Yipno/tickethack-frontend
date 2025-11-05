820607a49e0a0980ea2e37033e455b213bdb7b91
Aubry Gamard <aubry.soriano@gmail.com>
2025-11-05 19:12:45 +0400

    ajout fonction total et delete

--- cart.js
+++ cart.js
@@ -12,7 +12,7 @@ async function getCart() {
   const data = await result.json();
   const trips = data.cart;
   console.log(trips);
-  if (trips.length === 0) {
+  if (!trips.result) {
     container.innerHTML = `<p>No tickets in your cart.</p>`;
     return;
   }
@@ -23,14 +23,52 @@ async function getCart() {
     const horaire = moment(trips[i].trip.date).format("LT");
     html += `
             <div class='cart-item'>
-            <span>${trips[i].trip.departure} > ${trips[i].trip.arrival}</span>
-            <span class='horaire'>${horaire}</span>
-            <span class='price'>${trips[i].trip.price}€</span>
-            <span class='tripId' style='display: none;'>${trips[i].trip._id}</span>
-            <button class='delete'>
-            ❌
-                </button>
-                </div>`;
+              <span>${trips[i].trip.departure} > ${trips[i].trip.arrival}</span>
+              <span class='horaire'>${horaire}</span>
+              <span class='price'>${trips[i].trip.price}€</span>
+              <span class='cartId' style='display: none;'>${trips[i]._id}</span>
+              <span class='tripId' style='display: none;'>${trips[i].trip._id}</span>
+              <button class='remove-btn'>
+              ❌
+              </button>
+            </div>`;
   }
-  container.innerHTML = html;
+  container.innerHTML =
+    html +
+    `<div class="cart-total">
+        <span> Total : <span id="cart-total">0</span>€</span>
+        <button class="purchase-btn" id="purchase-btn">Purchase</button>
+    </div>`;
+  getTotal();
+  deleteCartItem();
 }
+
+function getTotal() {
+  const priceDiv = document.querySelectorAll(".price");
+  let total = 0;
+  priceDiv.forEach((price) => (total += parseInt(price.textContent)));
+  document.querySelector("#cart-total").textContent = total;
+}
+
+function deleteCartItem() {
+  const deleteBtns = document.querySelectorAll(".remove-btn");
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", async function () {
+      const cartId = this.parentNode.querySelector(".cartId").textContent;
+      // console.log(cartId);
+      const data = await fetch(`http://localhost:3000/cart/${cartId}`, {
+        method: "DELETE",
+      });
+      const result = await data.json();
+      console.log(result);
+      if (result.result) {
+        this.parentNode.remove();
+        getTotal();
+      } else {
+        alert("Cant touch this !");
+      }
+    });
+  });
+}
+
+// function deleteCartFromDB(id) {}
